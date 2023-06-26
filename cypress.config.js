126d95d1df212d6173d289f47cd9b117541057ef
my-cosm <monika.yadav@cosm.com>
2023-06-26 18:04:47 +0530

    Commit for mabda test and browser stack

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,7 @@
-const { defineConfig } = require("cypress");
+const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
-  integration: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+    e2e: {
+        setupNodeEvents(on, config) {}
+    }
 });
